b934491136fbcc34940652a7953a44f5ddf30bc3
Jamal Nasir <jamc_2@hotmail.com>
2026-03-12 14:34:17 +0500

    Got pull and fixed build issues of Select OTP Method and Enter OTP screens.

--- app/auth/otp-method/page.tsx
+++ app/auth/otp-method/page.tsx
@@ -1,29 +1,22 @@
-'use client'
-import { OtpMethod } from "@/components/packiq/auth/otp-method";
 import { OtpMethodIntegrated } from "@/components/packiq/auth/otp-method-integrated";
-import { useSearchParams  } from 'next/navigation'
 
-export default function OtpMethodPage(){
+type PageProps = {
+  searchParams: Promise<{
+    temptoken?: string;
+    email?: string;
+  }>;
+};
 
-      const searchParams  = useSearchParams();
-    
-      const temptoken = searchParams.get('temptoken');
-      const email = searchParams.get('email');
-     
-    
-     
+export default async function OtpMethodPage({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const temptoken = params?.temptoken;
+  const email = params?.email;
 
-    return(
-
-        <>
-        
-                <div className=" flex min-h-[calc(100vh-64px)] flex-col items-center justify-center p-6 md:p-10">
-                    <div className="w-full max-w-sm md:max-w-1xl">
-                      {/*<OtpMethod/>*/}
-                      <OtpMethodIntegrated temptoken={temptoken} email={email}/>
-                      </div>
-                      </div>
-        
-        </>
-    )
+  return (
+    <div className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-1xl">
+        <OtpMethodIntegrated temptoken={temptoken} email={email} />
+      </div>
+    </div>
+  );
 }
--- app/next-js-integrated/enter-otp/page.tsx
+++ app/next-js-integrated/enter-otp/page.tsx
@@ -9,12 +9,12 @@ import { useSearchParams } from 'next/navigation';
 
 export default function EnterOtpPage() {
 
-    const searchParams = useSearchParams();
+    //const searchParams = useSearchParams();
 
   return (
     <div className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-1xl">
-        <EnterOtpGetToken userId={''} otpmethod={''} />
+        {/*<EnterOtpGetToken userId={''} otpmethod={''} />*/}
        
       </div>
     </div>
